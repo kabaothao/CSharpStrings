3a38d40039464fdaa89b309a396b5380618dbea8
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-10 11:05:20 -0600

    update notes

--- CSharpStrings/Program.cs
+++ CSharpStrings/Program.cs
@@ -15,17 +15,22 @@ namespace StaticClassAttributes //same name as the project we are using. we are 
             Console.WriteLine(phrase.Contains("Academy")); //tell us if this phrase string contains a set of characters. you need a parameter. we want to find out if this phrase has a string called academy.
             Console.WriteLine(phrase.Contains("Academies")); //vice versa
 
-            Console.WriteLine(phrase[0]); //i can actually access each character.it should show G
-            Console.WriteLine(phrase[2]); //i can actually access each character.it should show r
-            Console.WriteLine(phrase[8]); //i can actually access each character.it should show A
+            Console.WriteLine(phrase[0]); //i can actually access each character. Console should show G
+            Console.WriteLine(phrase[2]); //i can actually access each character. Console should show r
+            Console.WriteLine(phrase[8]); //i can actually access each character. Console should show A
 
             //we can use IndexOf() method
-            //not only a string contain s certain value but it will tell us where this string is located.
+            //this will tell us where each character is located.
 
             Console.WriteLine(phrase.IndexOf("Academy"));   //so we know it start at 8
             Console.WriteLine(phrase.IndexOf("f"));   //so we know it start at 4
             Console.WriteLine(phrase.IndexOf("z"));   //so when we run the program it will be a - 1 number because that just mean this character is not inside the string.
 
+
+            //substring() method
+            Console.WriteLine(phrase.Substring(8));   //basically you can grab one part of the string. i tiwll grab starting at 8 and until the end of the string.
+            Console.WriteLine(phrase.Substring(8, 3));   //you can grab another character. so you're saying that you want to grab from 8 to 3. In your Console it would print out as Aca.
+                                                      
 
 
 
